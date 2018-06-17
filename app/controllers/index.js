05b0a992aa4a7797e8b318680df5bdb0b15c5de6
Jonathan Forrider <jd4rider@gmail.com>
2018-06-17 09:53:46 -0600

    Second

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -12,7 +12,6 @@ export default Controller.extend({
   isDisabled: not('isValid'),
 
   actions: {
-
     saveInvitation() {
       alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
       this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
